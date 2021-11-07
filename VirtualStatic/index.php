f8cf1bf5e01d485ae942bc864ccd5b4468c873a9
Kartik Verma <56619907+Kar2410@users.noreply.github.com>
2021-11-07 10:38:16 +0530

    Update index.php

--- VirtualStatic/index.php
+++ VirtualStatic/index.php
@@ -45,7 +45,7 @@ include("../includes/main.php");
   <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="shortcut icon" href="//cdn.shopify.com/s/files/1/2484/9148/files/SDQSDSQ_32x32.png?v=1511436147" type="image/png">
+
   <title>Team Trozans</title>
   <link href="../styles/bootstrap.min.css" rel="stylesheet">
   <link href="../styles/backend.css" rel="stylesheet">
