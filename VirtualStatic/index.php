660850d36f0ee62ec1c9dd982e11fbdf7a32cd81
Kartik Verma <56619907+Kar2410@users.noreply.github.com>
2021-11-07 10:39:49 +0530

    Update index.php

--- VirtualStatic/index.php
+++ VirtualStatic/index.php
@@ -26,7 +26,7 @@ session_start();
 
 include("../includes/db.php");
 include("../functions/functions.php");
-include("../includes/main.php");
+
 
 
 
@@ -45,7 +45,6 @@ include("../includes/main.php");
   <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
   <title>Team Trozans</title>
   <link href="../styles/bootstrap.min.css" rel="stylesheet">
   <link href="../styles/backend.css" rel="stylesheet">
@@ -55,6 +54,108 @@ include("../includes/main.php");
   </head>
 
 
+
+
+
+ <header class="page-header">
+    <!-- topline -->
+    <div class="page-header__topline">
+      <div class="container clearfix">
+
+        <div class="currency">
+          <a class="currency__change" href="customer/my_account.php?my_orders">
+          <?php
+          if(!isset($_SESSION['customer_email'])){
+          echo "Welcome :Guest"; 
+          }
+          else
+          { 
+              echo "Welcome : " . $_SESSION['customer_email'] . "";
+            }
+?>
+          </a>
+        </div>
+
+        <div class="basket">
+          <a href="cart.php" class="btn btn--basket">
+            <i class="icon-basket"></i>
+            <?php items(); ?> items
+          </a>
+        </div>
+        
+        
+        <ul class="login">
+
+<li class="login__item">
+<?php
+if(!isset($_SESSION['customer_email'])){
+  echo '<a href="customer_register.php" class="login__link">Register</a>';
+} 
+  else
+  { 
+      echo '<a href="customer/my_account.php?my_orders" class="login__link">My Account</a>';
+  }   
+?>  
+</li>
+
+
+<li class="login__item">
+<?php
+if(!isset($_SESSION['customer_email'])){
+  echo '<a href="checkout.php" class="login__link">Sign In</a>';
+} 
+  else
+  { 
+      echo '<a href="./logout.php" class="login__link">Logout</a>';
+  }   
+?>  
+  
+</li>
+</ul>
+      
+      </div>
+    </div>
+    <!-- bottomline -->
+    <div class="page-header__bottomline">
+      <div class="container clearfix">
+
+        <div class="logo">
+          <a class="logo__link" href="../index.php" style="text-decoration: none;">
+            <h2>TEAM TROZANS</h2>
+            <!-- <img class="logo__img" src="images/logo.png" alt="Avenue fashion logotype" width="237" height="19"> -->
+          </a>
+        </div>
+
+        <nav class="main-nav">
+          <ul class="categories">
+
+            <li class="categories__item">
+              <a class="categories__link" href="../shop.php">
+                <h4>Mens</h4>
+               
+              </a>
+              </li>
+
+            <li class="categories__item">
+              <a class="categories__link" href="../shop.php">
+                <h4>Womens</h4>
+               
+              </a>
+            </li>
+
+            <li class="categories__item">
+              <a class="categories__link categories__link--active" href="../shop.php">
+                 <h4>Shop</h4>
+              </a>
+            </li>
+
+      
+
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 
 
 
